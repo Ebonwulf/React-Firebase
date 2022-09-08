933b80e37f516c427970058976b192560a7f1f3e
ebonwulf <ebonwolf00@gmail.com>
2022-09-08 09:35:44 +0100

    save and push

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import './NewEventForm.css'
+import { useState } from 'react';
+import './NewEventForm.css';
 
 export default function NewEventForm() {
-  const [title, setTitle] = useState('')
-  const [date, setDate] = useState('')
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
 
   // const handleChange = (e) => {
   //   console.log(e.target.value)
   // }
 
   return (
-    <form className="new-event-form">
+    <form className='new-event-form'>
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input type='text' onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <input type='date' onChange={(e) => setDate(e.target.value)} />
       </label>
       <button>Submit</button>
-      <p>title - {title}, date - {date}</p>
+      <p>
+        title - {title}, date - {date}
+      </p>
     </form>
-  )
+  );
 }
