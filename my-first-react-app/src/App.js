a89875d7b01520de054526e7a5c30ee59c17bfb3
ebonwulf <ebonwolf00@gmail.com>
2022-09-08 09:03:23 +0100

    comments added to explain children props

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,28 +1,28 @@
-import './App.css'
-import React, { useState } from 'react'
-import Title from './components/Title'
-import Modal from './components/Modal'
+import './App.css';
+import React, { useState } from 'react';
+import Title from './components/Title';
+import Modal from './components/Modal';
 
 function App() {
-  const [showEvents, setShowEvents] = useState(true)
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "bowser's live stream", id: 2},
-    {title: "race on moo moo farm", id: 3}
-  ])
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: 'race on moo moo farm', id: 3 },
+  ]);
 
   const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
-    })
-  }
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => id !== event.id);
+    });
+  };
 
-  const subtitle = "All the latest events in Marioland"
+  const subtitle = 'All the latest events in Marioland';
 
   return (
-    <div className="App">
-      <Title title="Marioland Events" subtitle={subtitle} />
-      
+    <div className='App'>
+      <Title title='Marioland Events' subtitle={subtitle} />
+
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
@@ -33,21 +33,30 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
-      {showEvents && events.map((event, index) => (
-        <React.Fragment key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
-        </React.Fragment>
-      ))}
+      {showEvents &&
+        events.map((event, index) => (
+          <React.Fragment key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </React.Fragment>
+        ))}
 
       {/* <Modal>
         <h2>10% Off Coupon Code!!</h2>
         <p>Use the code NINJA10 at the checkout.</p>
       </Modal> */}
       <Modal>
+        {/*The h2 an p elements inside this opening and closing modal component are the childern prop that is passed down to the modal component*/}
         <h2>Terms and Conditions</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit nam et reprehenderit quibusdam temporibus officia dolorum quo sint nemo quis, laborum, quasi nisi fugit praesentium debitis repudiandae! Sapiente, omnis.</p>
-        <a href="#">find out more...</a>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit
+          nam et reprehenderit quibusdam temporibus officia dolorum quo sint
+          nemo quis, laborum, quasi nisi fugit praesentium debitis repudiandae!
+          Sapiente, omnis.
+        </p>
+        <a href='#'>find out more...</a>
       </Modal>
     </div>
   );
