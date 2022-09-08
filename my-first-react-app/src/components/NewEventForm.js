e72d7d65316a1bda36e5b5c3e1e192c9cf5bed81
ebonwulf <ebonwolf00@gmail.com>
2022-09-08 09:27:56 +0100

    comments added to explain form elements

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -1,19 +1,20 @@
-import './NewEventForm.css'
+import './NewEventForm.css';
 
 export default function NewEventForm() {
   return (
-    <form className="new-event-form">
+    <form className='new-event-form'>
       {/* <label htmlFor="title">Event Title:</label>
       <input type="text" id="title" /> */}
+      {/*wrapping the input element inside the label element means it will automatically be connected to the label, this works for anything you put inside the label and means you don't need to give each element an id and the label a htmlfor to connect them together*/}
       <label>
         <span>Event Title:</span>
-        <input type="text" />
+        <input type='text' />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" />
+        <input type='date' />
       </label>
       <button>Submit</button>
     </form>
-  )
+  );
 }
